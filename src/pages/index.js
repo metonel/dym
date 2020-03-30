72b9c175c17500614bb8ef19a0357f87e5b186ba
Antonio Nemtisor <nemtisor.antonio@gmail.com>
2020-03-30 07:42:39 +0300

    added personal page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Landing from '../sections/Landing';
 import About from '../sections/About';
+import Personal from '../sections/Personal';
 import Projects from '../sections/Projects';
 import Writing from '../sections/Writing';
 import Header from '../components/Header';
@@ -12,6 +13,7 @@ const IndexPage = () => (
     <Header />
     <Landing />
     <About />
+    <Personal />
     <Projects />
     <Writing />
     <Footer />
